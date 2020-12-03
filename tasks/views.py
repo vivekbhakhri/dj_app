491bea92ad58bcfc6131eb2f778593372511eabb
Vivek Bhakhri <vivekbhakhri739@gmail.com>
2020-12-03 02:49:15 -0500

    startedadding sessions

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,18 @@
 from django import forms
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
-tasks = ["foo", "bar", "baz"]
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request,"tasks/index.html",{
-        "tasks":tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -19,6 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form":form
