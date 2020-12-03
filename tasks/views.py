c3e5ee8969b25b1b8f169516994d9d0cea2aea13
Vivek Bhakhri <vivekbhakhri739@gmail.com>
2020-12-03 01:38:44 -0500

    added venv and some client side validation

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 tasks = ["foo", "bar", "baz"]
@@ -15,5 +15,5 @@ def index(request):
 
 def add(request):
     return render(request, "tasks/add.html",{
-        "form":NewTaskForm
+        "form":NewTaskForm()
     })
