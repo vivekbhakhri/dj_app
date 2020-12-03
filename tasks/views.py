3ff89e780eb094e059ac5bf24b537dff68153c02
Vivek Bhakhri <vivekbhakhri739@gmail.com>
2020-12-03 02:58:33 -0500

    added sessions

--- tasks/views.py
+++ tasks/views.py
@@ -9,7 +9,7 @@ class NewTaskForm(forms.Form):
 # Create your views here.
 def index(request):
     if "tasks" not in request.session:
-        request.session["tasks"] = []
+        request.session["tasks"] = []  #checking if session have an task list
 
     return render(request,"tasks/index.html",{
         "tasks": request.session["tasks"]
@@ -20,7 +20,7 @@ def add(request):
         form=NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"]+= [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
