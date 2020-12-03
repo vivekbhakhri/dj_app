99c91cc8af61e54cf8630ddb0d4e604d215ff16c
Vivek Bhakhri <vivekbhakhri739@gmail.com>
2020-12-03 02:25:31 -0500

    removed priority tag

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 tasks = ["foo", "bar", "baz"]
@@ -14,6 +14,17 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form=NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form":form
+            })
+
+
     return render(request, "tasks/add.html",{
         "form":NewTaskForm()
     })
